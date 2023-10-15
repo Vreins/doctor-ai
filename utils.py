ef8ec1884aedf7aaf1e55d90b28ec6840811bb1a
Adah Augustine <augustineadah67@gmail.com>
2023-10-15 10:58:07 +0100

    update utils and requirement

--- utils.py
+++ utils.py
@@ -1,14 +1,16 @@
-import pyaudio
+# import pyaudio
 import speech_recognition as sr
 
 
-def get_mic_index(name):
-    for i in range(pyaudio.PyAudio().get_device_count()):
-        info = pyaudio.PyAudio().get_device_info_by_index(i)
-        if info.get('name') == name:
-            return i
+# def get_mic_index(name):
+#     for i in range(pyaudio.PyAudio().get_device_count()):
+#         info = pyaudio.PyAudio().get_device_info_by_index(i)
+#         if info.get('name') == name:
+#             return i
 
 
 recognizer = sr.Recognizer()
-device_index = get_mic_index("Ecamm Live Virtual Mic")
-microphone = sr.Microphone(device_index=device_index)
+# device_index = get_mic_index("Ecamm Live Virtual Mic")
+microphone = sr.Microphone(
+    # device_index=device_index
+    )
